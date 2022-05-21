1ca6612eeefb9338f3abbef7b37ae5bc06c62551
Boris <boris@randebrock.com>
2022-05-21 21:39:30 +0200

    added userControllers

---
+++ src/server/controllers/userControllers.js
@@ -0,0 +1,40 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../../db/models/User");
+
+const loginUser = async (req, res, next) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    const error = new Error();
+    error.statusCode = 403;
+    error.customMessage = "Incorrect username or password";
+
+    next(error);
+  }
+  const correctPassword = await bcrypt.compare(password, user.password);
+  if (!correctPassword) {
+    const error = new Error();
+    error.statusCode = 403;
+    error.customMessage = "Incorrect username or password";
+
+    next(error);
+  }
+  try {
+    const userData = {
+      username,
+      id: user.id,
+    };
+    const token = jwt.sign(userData, process.env.JWT_SECRET);
+
+    res.status(200).json({ token });
+  } catch (error) {
+    error.customMessage = "invalid user data";
+    error.statusCode = 400;
+
+    next(error);
+  }
+};
+
+module.exports = { loginUser };
