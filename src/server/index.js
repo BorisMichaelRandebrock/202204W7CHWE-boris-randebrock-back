045e91c0ea6de6da270612fc96e3ad1292317175
Boris <boris@randebrock.com>
2022-05-22 14:59:52 +0200

    added cors and moked users

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { default: helmet } = require("helmet");
 const morgan = require("morgan");
+const cors = require("cors");
 const { notFoundError, generalError } = require("./middlewares/errors");
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.json());
