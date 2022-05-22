a006c09907b7c8336b03be9304d5b9f42646c54f
Boris <boris@randebrock.com>
2022-05-22 13:54:45 +0200

    100 on testing userControllers

--- src/server/controllers/userControllers.test.js
+++ src/server/controllers/userControllers.test.js
@@ -1,17 +1,17 @@
+const bcrypt = require("bcrypt");
+const User = require("../../db/models/User");
 const { loginUser } = require("./userControllers");
 
 const expectedToken = "YouKilledMyFather";
+
 const user = {
   username: "IñigoMontoya",
+  password: "correctPassord",
   id: 13,
 };
 
 jest.mock("../../db/models/User", () => ({
   findOne: jest.fn().mockResolvedValue(user),
-}));
-
-jest.mock("bcrypt", () => ({
-  compare: jest.fn().mockResolvedValue(true),
 }));
 
 jest.mock("jsonwebtoken", () => ({
@@ -24,26 +24,90 @@ describe("Given the loginUser function", () => {
     json: jest.fn(),
   };
 
-  const req = {
-    body: {
-      username: "JuanCa",
-      password: "bribon",
-    },
-  };
-
+  jest.mock("bcrypt", () => ({
+    compare: jest.fn().mockResolvedValue(true),
+  }));
   describe("When invoked with the correct username", () => {
-    test("Then it should return the error.statusCode 200", async () => {
+    const req = {
+      body: {
+        username: "JuanCa",
+        password: "bribon",
+      },
+    };
+    test("Then it should respond the statusCode 200", async () => {
       const expectedStatusCode = 200;
-      await loginUser(req, res);
+      const next = jest.fn();
 
+      await loginUser(req, res, next);
       expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
     });
     test("Then it should respond with the res.json method with the message 'YouKilledMyFather'", async () => {
       const expected = { token: "YouKilledMyFather" };
+      const next = jest.fn();
 
-      await loginUser(req, res);
+      await loginUser(req, res, next);
 
       expect(res.json).toBeCalledWith(expected);
+    });
+  });
+  describe("When invoked with an incorrect password", () => {
+    test("Then it should call next", async () => {
+      jest.spyOn(bcrypt, "compare").mockResolvedValue(false);
+      User.findOne = jest.fn().mockResolvedValue(user);
+
+      const req = {
+        body: {
+          username: "JuanCa",
+          id: "1",
+          password: "bribon",
+        },
+      };
+
+      const next = jest.fn();
+
+      await loginUser(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+  });
+  describe("When invoked with an incorrect username", () => {
+    test("Then it should call next", async () => {
+      jest.spyOn(bcrypt, "compare").mockResolvedValue(true);
+      User.findOne = jest.fn().mockResolvedValue(!user);
+
+      const req = {
+        body: {
+          username: "JuanCa",
+          id: "1",
+          password: "bribon",
+        },
+      };
+
+      const next = jest.fn();
+
+      await loginUser(req, res, next);
+
+      expect(next).toHaveBeenCalled();
+    });
+    test("Then it should call next", async () => {
+      jest.spyOn(bcrypt, "compare").mockResolvedValue(true);
+      User.findOne = jest.fn().mockResolvedValue(!user);
+      const error = new Error();
+      error.customMessage = "invalid user Data";
+
+      const req = {
+        body: {
+          username: "JuanCa",
+          id: "1",
+          password: "bribon",
+        },
+      };
+
+      const next = jest.fn();
+
+      await loginUser(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(error);
     });
   });
 });
